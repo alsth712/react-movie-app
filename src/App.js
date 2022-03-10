b36a239d28f16fc45006d8d1722c1e09461da6a9
alsth712 <alsth712@naver.com>
2022-03-10 20:53:44 +0900

    🔨 App.js 파일 내용 이동

--- src/App.js
+++ src/App.js
@@ -1,48 +1,8 @@
 import { useEffect, useState } from "react";
+import Movie from "./components/Movie";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  // async-await 를 then보다 많이 사용하므로, async-await을 사용하기 위해서 getMovies 라는 함수를 만들어줄 것
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  return (
-    <div>
-      {loading ? (
-        <h1>Loading...⏰</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} />
-              <h2>{movie.title}</h2>
-              <h5>
-                year : {movie.year} / rating : ⭐{movie.rating}
-              </h5>
-              <p>{movie.summary}</p>
-              {movie.hasOwnProperty("genres") ? (
-                <ul>
-                  {movie.genres.map((g) => (
-                    <li key={g}>{g}</li>
-                  ))}
-                </ul>
-              ) : null}
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return null;
 }
 
 export default App;
