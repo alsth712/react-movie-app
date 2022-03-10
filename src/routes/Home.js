ba697460b974d8bc0c44d1b9a7431eaa15fdcfe6
alsth712 <alsth712@naver.com>
2022-03-10 20:57:20 +0900

    🔨 Home.js 파일 생성

---
+++ src/routes/Home.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import Movie from "../components/Movie";
+
+function Home() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  // async-await 를 then보다 많이 사용하므로, async-await을 사용하기 위해서 getMovies 라는 함수를 만들어줄 것
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...⏰</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              year={movie.year}
+              rating={movie.rating}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Home;
