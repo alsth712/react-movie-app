c5db94212f330a78ff2dff9c2d683c0c1ab16e88
alsth712 <alsth712@naver.com>
2022-03-30 01:55:40 +0900

    🔨 Route 경로 변경

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Detail from "./routes/Detail";
+import Genre from "./routes/Genre";
+import HighRate from "./routes/HighRate";
 import Home from "./routes/Home";
 
 function App() {
+  const [genres, setGenres] = useState(["action", "animation", "music"]);
+
   return (
     <Router>
+      <Navbar />
       <Switch>
+        <Route path={`/movie/highRating`}>
+          <HighRate rate={8.5} />
+        </Route>
+        {genres.map((genre, index) => {
+          return (
+            <Route path={`/movie/${genre}`}>
+              <div>
+                <Genre key={index} genre={genre} />
+              </div>
+            </Route>
+          );
+        })}
         <Route path="/movie/:id">
           <Detail />
         </Route>
