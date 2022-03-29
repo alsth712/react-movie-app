c270dfa4b2932339c48c73051cb7ac08e9bfa3d6
alsth712 <alsth712@naver.com>
2022-03-30 01:54:00 +0900

    🔨 HighRate.js 파일 생성

---
+++ src/routes/HighRate.js
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import Loading from "../components/Load";
+import Movie from "../components/Movie";
+
+function HighRate({ rate }) {
+  // state
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=${rate}&sort_by=download_count`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className={`${rate} rate`}>
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              id={movie.id}
+              backImg={movie.background_image}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              year={movie.year}
+              rating={movie.rating}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default HighRate;
