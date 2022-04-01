db43199908d6724c8635d8fd44a39cfdb7d534a4
alsth712 <alsth712@naver.com>
2022-04-01 13:54:35 +0900

    🔨 home link 위치 수정

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,36 @@
 import { Link } from "react-router-dom";
+import styles from "../styles/Navbar.module.css";
 
 // HOME, menu bar
 function Navbar() {
   return (
     <header>
-      <div>
-        <Link to={`/`}>sofilx</Link>
-      </div>
       <nav>
-        <ul>
+        <ul className={styles.navbar}>
           <li>
-            <Link to={`/movie/highRating`}>High Rating</Link>
+            <Link className={styles.home} to={`/`}>
+              sofilx
+            </Link>
           </li>
           <li>
-            <Link to={`/movie/action`}>Action</Link>
+            <Link className={styles.link} to={`/movie/highRating`}>
+              High Rating
+            </Link>
           </li>
           <li>
-            <Link to={`/movie/animation`}>Animation</Link>
+            <Link className={styles.link} to={`/movie/action`}>
+              Action
+            </Link>
           </li>
           <li>
-            <Link to={`/movie/music`}>Music</Link>
+            <Link className={styles.link} to={`/movie/animation`}>
+              Animation
+            </Link>
+          </li>
+          <li>
+            <Link className={styles.link} to={`/movie/music`}>
+              Music
+            </Link>
           </li>
         </ul>
       </nav>
