6e7e0aa54a45e1043460691de9cfe5d3bec2392e
alsth712 <alsth712@naver.com>
2022-04-01 13:53:33 +0900

    🔨 Load.js 수정

--- src/components/Load.js
+++ src/components/Load.js
@@ -1,8 +1,10 @@
+import styles from "../styles/Load.module.css";
+
 // loading 화면
 function Loading() {
   return (
-    <div>
-      <h1>Loading...⏰</h1>
+    <div className={styles.wrap}>
+      <div className={styles.LoadBox}></div>
     </div>
   );
 }
