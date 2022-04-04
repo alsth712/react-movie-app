cbc0c5fe7896ffec5cc53ccde9553a773c353b7e
alsth712 <alsth712@naver.com>
2022-04-05 00:00:47 +0900

    🔨 class명 추가 및 변경

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,11 +1,14 @@
 // useParams 은 react-router-dom에서 제공하는 함수로 url에 있는 변수 값을 반환
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Loading from "../components/Load";
+import styles from "../styles/Detail.module.css";
 
 function Detail() {
   const { id } = useParams();
   const [loading, setLoading] = useState(true);
   const [detail, setDetail] = useState();
+
   // await는 async함수 내부에 있어야만 함
   const getMovie = async () => {
     const json = await (
@@ -14,26 +17,45 @@ function Detail() {
     setDetail(json.data.movie);
     setLoading(false);
   };
+
+  // useEffect
   useEffect(() => {
     getMovie();
   }, []);
+
+  // Movie Detail
   return (
-    <div>
+    <div className={styles.container}>
       {loading ? (
-        <h1>Detail</h1>
+        <Loading />
       ) : (
-        <div>
-          <div>navbar 만들기 ~ 홈, about, detail 이렇게 !</div>
-          <img src={detail.large_cover_image} alt={detail.title} />
-          <h1>{detail.title}</h1>
-          <h3>year : {detail.year}</h3>
-          <div>
-            genres :
-            {detail.genres.map((g) => (
-              <span key={g}> {g} | </span>
-            ))}
+        <div
+          className={styles.backImg}
+          style={{
+            background: `url(${detail.background_image})`,
+            backgroundRepeat: `no-repeat`,
+            backgroundSize: `cover`,
+            backgroundPosition: `center`,
+            overflow: `hidden`,
+          }}
+        >
+          <div className={styles.detailBox}>
+            <img
+              className={styles.mainImg}
+              src={detail.large_cover_image}
+              alt={detail.title}
+            />
+            <h1 className={styles.title}>{detail.title}</h1>
+            <h3 className={styles.year}>year : {detail.year}</h3>
+            <div className={styles.genres}>
+              <ul>
+                {detail.genres.map((g) => (
+                  <li key={g}>🎬{g}</li>
+                ))}
+              </ul>
+            </div>
+            <p className={styles.summary}>{detail.description_intro}</p>
           </div>
-          <p>{detail.description_intro}</p>
         </div>
       )}
     </div>
